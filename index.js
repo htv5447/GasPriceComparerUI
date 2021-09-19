1fc2b9868a4977eb900259b90e5a77b87bce18cb
Huong Vu <htv5447@rit.edu>
2021-09-19 02:15:18 -0400

    2nd page

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const searchButton = document.getElementById('search-button');
-const searchInput = document.getElementById('search-input');
-searchButton.addEventListener('click', () => {
-  const inputValue = searchInput.value;
-  alert(inputValue);
-});
+// const searchButton = document.getElementById('search-button');
+// const searchInput = document.getElementById('search-input');
+// searchButton.addEventListener('click', () => {
+//   const inputValue = searchInput.value;
+//   alert(inputValue);
+// });
